ef8f9fcf7bb4073f9f31a5eb340de2ddb0e1bedd
Alan <Perez@Alans-MacBook-Pro.local>
2019-12-02 00:07:10 -0600

    cambio de ruta de BD

--- DBConnection.js
+++ DBConnection.js
@@ -3,7 +3,7 @@ let mysql = require('mysql');
 
 function sqlQuery(query, values, callback){
     var connection = mysql.createConnection({
-    host     : '127.0.0.1',
+    host     : '157.245.233.250',
     user     : 'root',
     password : 'Cocacola123.',
     database : 'hydroconnectApp'
